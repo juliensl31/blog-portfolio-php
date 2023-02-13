34866efc5360c17f9af73ad80daf1f54caaa6a0b
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2023-02-13 19:29:57 +0100

    fix bootstrap

--- view/articleView.php
+++ view/articleView.php
@@ -7,13 +7,10 @@ ob_start();
 <section class="container">
 
     <h1 class="text-light mt-5 text-decoration-underline">ARTICLE</h1>
-
-    <?php
-    while ($article = $requete->fetch()) {
-    ?>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-            <div class="col">
-                <div class="card border-light bg-dark text-light mb-3 h-100">
+    <?php while ($article = $requete->fetch()) { ?>
+        <div class="row">
+            <div class="col mb-3">
+                <div class="card border-light bg-dark text-light h-100">
                     <div class="card-header fw-bold">Blog</div>
                     <div class="card-body">
                         <h5 class="card-title"><?= $article['title'] ?></h5>
@@ -21,20 +18,16 @@ ob_start();
                     </div>
                     <div class="card-footer">
                         <p class="card-text text-end"><?= $article['created_date'] ?></p>
-                        <form method="post" action="index.php?page=article" class="delete_form text-center">
-                            <input type="hidden" name="id" value="<?php echo $article['id'] ?>"/>  
+                        <form method="post" action="index.php?page=article" class="text-end">
+                            <input type="hidden" name="id" value="<?php echo $article['id'] ?>" />
                             <button type="submit" name="update" class="bg-info text-light fw-bold border-0 p-3 rounded mb-2 text-uppercase">Modifier un article</button>
                             <button type="submit" name="delete" class=" delete bg-info text-light fw-bold border-0 p-3 rounded mb-2 text-uppercase">Supprimer un article</button>
                         </form>
                     </div>
                 </div>
-                
-
             </div>
         </div>
-    <?php
-    }
-    ?>
+    <?php } ?>
 
 </section>
 
